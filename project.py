4fe7bd9ad24b6e70b98e2a82577e6569732b120e
gavr1988 <gavrewt@gmail.com>
2025-12-28 11:48:57 +0000

    update

--- project.py
+++ project.py
@@ -16,7 +16,7 @@ print(df)
 print ("information about data frame") #getting a technical summary of the data frame
 print (df.info())
 
-#cleaning the data to form a single source of tru
+#cleaning the data to form a single source of truth
 
 #this will replace the NaN values in price
 
@@ -96,6 +96,61 @@ growth = daily_revenue - daily_revenue.shift(1)
 
 print (growth)
 
+#Merge and concatenate data
+
+new_data = {
+    'Date': ['2023-10-04', '2023-10-04'],
+    'Item': ['Latte', 'Muffin'],
+    'Price': [4.50, 4.10],
+    'Quantity': [2, 1],
+    'Barista': ['Alice', 'Bob'],
+    'Revenue': [9.00, 4.10]
+}
+
+#1st convert to new data frame
+
+df_new_day = pd.DataFrame (new_data)
+
+#convert date to date.time (Clean new data)
+
+df_new_day['Date'] = pd.to_datetime(df_new_day['Date'])
+
+df_new_day.set_index('Date',inplace=True)
+
+print ("New Data: ")
+
+print (df_new_day)
+#Concatenating df_new_day with orignial date - you must state if it is vertucal or horizontal concat. axis = 0 is new row)
+
+full_history = pd.concat([df,df_new_day], axis=0)
+                         
+print ("Full data")
+
+print (full_history)
+
+print ('========Merging Cost data===============')
+
+#create new data frame df_costs
+cost_data = {
+    'Item': ['Latte', 'Espresso', 'Muffin', 'Cappuccino'],
+    'Cost': [1.50, 0.80, 1.00, 1.20]
+}
+df_costs = pd.DataFrame (cost_data)
+print ('Cost table')
+print (df_costs)
+
+#resetting the index
+
+full_history_reset = full_history.reset_index()
+
+df_with_costs = pd.merge(full_history_reset, df_costs, on='Item',how = 'left')
+#on looks in the cost table and matches it with the cost that is newly generated
+print ('Data Frame With costs after merging costs')
+
+print (df_with_costs)
+
+
+
 #using lambda function
 
 print (df)
@@ -105,3 +160,4 @@ print ('adding category column')
 df['Category'] = df['Revenue'].apply(lambda x: 'High' if x> 10 else 'Std')
 
 print (df)
+
